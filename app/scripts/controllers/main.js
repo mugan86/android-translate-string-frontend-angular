43820ab83260f7489b6a84d73c0b58415bf5cf48
Anartz Muxika Ledo <mugan86@gmail.com>
2016-12-16 08:22:25 +0100

    Call to translationsService functions

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,27 @@
  * Controller of the translationsApp
  */
 angular.module('translationsApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $http, translationsService) {
+    
+
+	//Show Translate APPs List Info
+    translationsService.getAppList()
+            .then(function(data) {
+
+           console.log(data);
+    });
+
+    //Show select APP Info       
+    translationsService.getSelectAppInfo('com.amugika.servirace')
+            .then(function(data) {
+
+           console.log(data);
+    });
+
+    //Show select APP Translate Text in second page with 6 results per page (dinamic)
+    translationsService.getSelectAppTranslateTexts('com.amugika.servirace', 2, 6)
+            .then(function(data) {
+
+           console.log(data);
+    });
   });
