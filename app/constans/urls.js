d27b0ab01491e41eb00fcaf4f5597b970d40446f
Anartz Muxika Ledo <mugan86@gmail.com>
2016-12-16 08:21:19 +0100

    Add constants directory and inside files to add constants

---
+++ app/constans/urls.js
@@ -0,0 +1,15 @@
+/**********************************************************************************************
+Constant values files
+----------------------------
+
+
+***********************************************************************************************/
+angular.module('translationsApp')
+
+.constant('LOCAL_LOCALHOST', 'http://192.168.110.131/translations/')
+.constant('F1', {
+                        main_url: 'http://ergast.com/api/f1/',
+                        drivers: 'drivers.json',
+                        circuits: 'circuits.json'
+                    })
+;
