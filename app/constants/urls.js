fa6704e2dc951b669b48f5c50b0da416e2c49674
anartzmugika <mugan86@gmail.com>
2016-12-16 19:17:07 +0100

    Home localhost url

--- app/constants/urls.js
+++ app/constants/urls.js
@@ -9,7 +9,8 @@
  */
 angular.module('translationsApp')
 
-.constant('LOCAL_LOCALHOST', 'http://192.168.110.131/translations/')
+/*.constant('LOCAL_LOCALHOST', 'http://192.168.110.131/translations/')*/
+.constant('LOCAL_LOCALHOST', 'http://192.168.1.5/translations/')
 .constant('F1', {
                         main_url: 'http://ergast.com/api/f1/',
                         drivers: 'drivers.json',
