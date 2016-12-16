e848ebe677bd9e01e8ffb7478223a9fa1783018c
Anartz Muxika Ledo <mugan86@gmail.com>
2016-12-16 10:56:07 +0100

    Add contact controller

---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name translationsApp.controller:ContactCtrl
+ * @description: To send contact data to contact with me.
+ * # ContactCtrl
+ * Controller of the translationsApp
+ */
+angular.module('translationsApp')
+  .controller('ContactCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
